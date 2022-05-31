dd4acadd986b451d97a093ebde946a35ce9be48a
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2022-05-30 22:36:09 -0300

    Análise de taxa de compressão

--- LZ78.py
+++ LZ78.py
@@ -1,8 +1,10 @@
 from Trie import Trie
+from os import stat
 
 def compression(filename, output):
   tree = Trie()
-  
+  highestCode = 0
+
   with open(filename, 'r') as inputFile:
     with open(output, 'w') as outputFile:
       pattern = ''
@@ -17,11 +19,27 @@ def compression(filename, output):
           continue
         else:
           prefixCode = tree.insert(pattern)
+
+          if prefixCode > highestCode: highestCode = prefixCode
+
           outputFile.write(f'{prefixCode}~{char}|')
           pattern = ''
       
       if pattern != '':
+        if prefixCode > highestCode: highestCode = prefixCode
         outputFile.write(f'{prefixCode}~{char}|')
+  
+  with open('./compressionReport.txt', 'w') as reportFile:
+    originalFileBitSize = stat(filename).st_size * 8
+
+    trieNodeCount = tree.nextCode - 1
+    outputFileBitSize = trieNodeCount * (highestCode.bit_length() + 8)
+
+    compressionRate = outputFileBitSize / originalFileBitSize
+
+    reportFile.write(f'Input: {filename} - Bit size: {originalFileBitSize}\n')
+    reportFile.write(f'Output: {output} - Bit size: {outputFileBitSize}\n')
+    reportFile.write(f'Taxa de compressão: {round(compressionRate, 2)}\n')
 
 def decompression(filename, output):
   with open(filename, 'r') as inputFile:
