434f8b95d30c3a977179d4e8f266cd4e14a749af
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2022-06-05 15:56:36 -0300

    Comentários

--- LZ78.py
+++ LZ78.py
@@ -1,54 +1,84 @@
 from Trie import Trie
 from os import stat
 
+# Compressão seguindo o LZ78
 def compression(filename, output):
   tree = Trie()
+  # Armazena o maior código de prefixo usado na saída para ser usado no cálculo da taxa
+  # de compressão. A quantidade de bits necessária para armazenar esse código será usada
+  # para todos os outros os códigos armazenados
   highestCode = 0
 
-  with open(filename, 'r') as inputFile:
-    with open(output, 'w') as outputFile:
+  with open(filename, 'r', encoding='utf-8') as inputFile:
+    with open(output, 'w', encoding='utf-8') as outputFile:
+      # Padrão que é buscado na Trie
       pattern = ''
       outputFile.write('|')
+
+      # Itera por todos os caracteres da entrada
       while True:
         char = inputFile.read(1)
-        if char == '': break
+        if char == '': break # Cláusula para verificar se chegou no final do arquivo
 
-        pattern = pattern + char
+        # Concatena o padrão com o atual caractere lido
+        pattern = ''.join((pattern, char))
         prefixCode = tree.find(pattern)
+
+        # Padrão existe na Trie. Acúmula o caractere e vai para a próxima iteração
         if prefixCode != -1:
           continue
+        # Padrão não existe na Trie, então deve ser inserido
         else:
+          # Insere o padrão na Trie e pega o código do prefixo
           prefixCode = tree.insert(pattern)
 
           if prefixCode > highestCode: highestCode = prefixCode
 
           outputFile.write(f'{prefixCode}~{char}|')
+          # Define o padrão como a string vazia
           pattern = ''
       
+      # É preciso fazer uma última verificação após while para tratar o caso em que
+      # o último caractere da entrada for acumulado no padrão, visto que, quando isso
+      # ocorre, ele não é gerado na saída no while.
       if pattern != '':
         if prefixCode > highestCode: highestCode = prefixCode
         outputFile.write(f'{prefixCode}~{char}|')
   
-  with open('./compressionReport.txt', 'w') as reportFile:
+  # Gerando o relatório com a taxa de compressão
+  with open('./compressionReport.txt', 'w', encoding='utf-8') as reportFile:
+    # Obtém o tamanho do arquivo original em bits (tamanho em bytes * 8)
     originalFileBitSize = stat(filename).st_size * 8
 
+    # Número de nós na Trie (sem contar a raiz)
     trieNodeCount = tree.nextCode - 1
+
+    # O cálculo do tamanho teórico do arquivo de saída é feito considerando que, para cada nó na Trie, será 
+    # salvo o código numérico do seu prefixo e o caractere associado ao nó. Dessa forma, para cada nó serão 
+    # gastos o número de bits necessário para armazenar o código mais 1 byte (8 bits) para o caractere
     outputFileBitSize = trieNodeCount * (highestCode.bit_length() + 8)
 
+    # A taxa de compressão considerada é a razão entre o tamanho do arquivo de saída e o arquivo de entrada
     compressionRate = outputFileBitSize / originalFileBitSize
 
     reportFile.write(f'Input: {filename} - Bit size: {originalFileBitSize}\n')
     reportFile.write(f'Output: {output} - Bit size: {outputFileBitSize}\n')
     reportFile.write(f'Taxa de compressão: {round(compressionRate, 2)}\n')
 
+# Descompressão seguindo o LZ78
 def decompression(filename, output):
-  with open(filename, 'r') as inputFile:
-    with open(output, 'w') as outputFile:
+  with open(filename, 'r', encoding='utf-8') as inputFile:
+    with open(output, 'w', encoding='utf-8') as outputFile:
+      # Faz o split da entrada (sem considerar o primeiro e o último caracteres)
       input = inputFile.read().split('|')[1:-1]
+      # Aplica um novo split em cada elemento da lista gerada pelo primeiro split
+      # para separar os códigos dos caracteres
       input = [x.split('~') for x in input]
 
+      # Lista auxiliar que contém os prefixos computados ao longo do for
       aux = ['']
       for index, char in input:
+        # A string gerada será o prefixo do elemento atual concatenado com o caractere associado a ele
         newStr = f'{aux[int(index)]}{char}'
         aux.append(newStr)
         outputFile.write(newStr)
--- Trie.py
+++ Trie.py
@@ -1,34 +1,59 @@
+# Classe que representa um nó da árvore Trie
 class Node:
   def __init__(self, symbol, code):
-    self.symbol = symbol
-    self.code = code
-    self.children = {}
+    self.symbol = symbol # Símbolo armazenado no nó
+    self.code = code # Código numérico associado ao nó
+    self.children = {} # Dicionário de filhos do nó. A chave é o símbolo armazenado no filho e o valor é o Node filho
 
+
+# Classe que representa uma árvore Trie tradicional
 class Trie:
   def __init__(self):
+    # Raiz da Trie. Armazena a string vazia e tem 0 como código
     self.root = Node("", 0)
+    # Código do próximo nó a ser armazenado na árvore. Inicialmente é 1
+    # Além disso também é equivalente ao número de nós na árvore
     self.nextCode = 1
   
+  # Método para inserção de palavra na árvore
+  # Retorna o código associado ao nó pai do nó inserido. Ou seja, o código do prefixo da palavra inserida
   def insert(self, word):
+    # Primeiramente, é preciso encontrar onde será inserido o nó para representar essa palavra
+    # Começa a busca pela raiz da árvore
     node = self.root
-    prefixCode = 0
+    # O valor inicial do código do prefixo é 0, ou seja, o prefixo ""
+    prefixCode = 0 
 
+    # Itera pelos caracteres da palavra a ser inserida
     for char in word:
+      # Se o caractere atual estiver associado a algum filho do nó atual da busca, então buscará o próximo caractere
+      # da palavra neste nó filho
       if char in node.children:
         node = node.children[char]
         prefixCode = node.code
+      # Caso contrário, é criado um nó para o caractere atual. É tido como premissa que só se cairá nesse caso quando
+      # se estiver no último caractere da palavra, visto que, quando essa função é chamada para palavras com mais de
+      # um caractere, o seu prefixo é formado por caracteres de nós na Trie
       else:
         newNode = Node(char, self.nextCode)
         node.children[char] = newNode
         self.nextCode += 1
         return prefixCode
   
+  # Método para buscar uma string na Trie
+  # Se a palavra for encontrada, retorna o código associado ao nó do último caractere, que é o código associado a essa palavra
+  # Caso contrário, retorna -1
   def find(self, word):
+    # Começa a busca pela raiz
     node = self.root
 
+    # Itera pelos caracteres da palavra
     for char in word:
+      # Se o caractere atual estiver associado a algum filho do nó atual da busca, então buscará o próximo caractere
+      # da palavra neste nó filho
       if char in node.children:
         node = node.children[char]
+      # Se, em algum momento, o caractere não estiver associado a nenhum nó filho, então indica que a palavra não está na Trie
       else:
         return -1
     
